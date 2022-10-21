6c17c305375055229139c460aceff2720a4dcff7
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-22 00:09:36 +0200

    fetch abi

---
+++ client/getAbi.js
@@ -0,0 +1,4 @@
+const erc20ABI = ["function decimals() external view returns(uint8)"];
+const factoryAbi = [
+    "function getPair(address tokenA, address tokenB) external view returns (address pair)"
+];
