5eb56e1296d38ecc4ccd05e0240dee0021907b4b
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-24 22:15:42 +0200

    remove id

--- client/fetchPrices.js
+++ client/fetchPrices.js
@@ -8,5 +8,5 @@ addressTo} = require ("./addresslist");
 const {erc20ABI, factoryAbi, pairABI, routerABI} = require("./getABI");
 
 // standard provider
-const INFURA_ID = '3d05647a39544dafab60d295c1ece741';
+const INFURA_ID = '';
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
