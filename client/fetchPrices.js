8e56ef9d1c8fe8e9f34957eb28ef16daea3c42da
kodakr <111961380+kodakr@users.noreply.github.com>
2022-10-24 22:16:16 +0200

    more code

--- client/fetchPrices.js
+++ client/fetchPrices.js
@@ -10,3 +10,28 @@ const {erc20ABI, factoryAbi, pairABI, routerABI} = require("./getABI");
 // standard provider
 const INFURA_ID = '';
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+
+// //connect to factory
+const contractFactory = new ethers.Contract(addressFactory,factoryAbi,provider);
+const contractRouter = new ethers.Contract(addressRouter,routerABI,provider);
+
+// connect to blockchain
+const getPrices = async (amountInHuman) => {
+    const contractToken = new ethers.Contract(addressFrom, erc20ABI,provider);
+    const decimal = await contractToken.decimals();
+    const amountIn = ethers.utils.parseUnits(amountInHuman, decimal).toString();
+    console.log(amountIn);
+    
+    // amountOut
+    const Out = await contractRouter.getAmountsOut(amountIn, [addressFrom,addressTo]);
+    
+   // amountsOut to decimal;
+   const ab2 = new ethers.Contract(addressTo, erc20ABI, provider);
+   const decimal2 = await ab2.decimals();
+   const decimalOut = ethers.utils.formatUnits(Out[1].toString(), decimal2);
+   console.log(decimalOut);
+}
+
+
+const amount = "1";
+getPrices(amount);
